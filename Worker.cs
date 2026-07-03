e5d7740e3443f4c11a096e8adbe5aa40da79100b
Mehmet Kadayıfçı <mkadayifci@gmail.com>
2026-07-03 22:39:55 +0300

    Deploy test

--- Worker.cs
+++ Worker.cs
@@ -68,6 +68,7 @@ public sealed class Worker : BackgroundService
         await client.ConnectAsync(options, stoppingToken);
 
         _logger.LogInformation("MQTT Connected");
+        _logger.LogInformation("This is the new version");
 
         await client.SubscribeAsync($"tronloop/node/{NodeId}/cmd", cancellationToken: stoppingToken);
         await client.SubscribeAsync("tronloop/broadcast/cmd", cancellationToken: stoppingToken);
